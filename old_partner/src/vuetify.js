821036f7976ac1152aece214e5e9ed50bcaa2a0c
Beezarment <armand.dufresne@epitech.eu>
2025-10-17 15:03:09 +0200

    feat: adding color palette for the project

--- old_partner/src/vuetify.js
+++ old_partner/src/vuetify.js
@@ -1,8 +1,22 @@
-import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
 
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          background: '#fafafa',
+          primary: '#4E342E',
+          secondary: '#F5E6CC',
+          accent: '#FF7043',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#2E7D32',
+          warning: '#FFC107',
+        },
+      },
+    },
   },
 })
